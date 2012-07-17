31c52a580a3b6bf20c3dc239f4e4a0762f4dcb14
Nicholas Wallen <nicholas.a.wallen@gmail.com>
2012-07-17 00:34:34 -0400

    Add touch-enabled slideshow of project media. Add froogaloop and hammer js libraries.

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -2,12 +2,18 @@
 Nicholas Wallen */
 
 jQuery(document).ready(function($) {
- 
+
+        /* scrolling events */
+         
+        //scroll to anchor on click
+         
 	    $(".scroll").click(function(event){		
 		    event.preventDefault();
              var hash =  this.hash;
 		    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500, '', function(){ location.hash = hash;});
         });
+
+        //update active project in nav menu
 
         $(".prt-project").waypoint(function(event,direction){
                 $('.prt-nav-link').removeClass('active');
@@ -35,6 +41,8 @@ jQuery(document).ready(function($) {
                 }
         },{offset: '70' });
 
+        //control visiblity of quick navigation menu 
+
          $("#prt-projects").waypoint(function(event,direction){
             if(direction =='down'){
                 $('#quick-nav').removeClass('hide');
@@ -53,5 +61,126 @@ jQuery(document).ready(function($) {
 		     event.preventDefault();
              $('#quick-nav').toggleClass('min');
          });
+
+
+         /* slideshow */
+
+         
+
+        // disable the dragging of images on desktop browsers
+        $(".prt-slideshow-container").children().bind("dragstart", function() { 
+            return false; 
+        });
+
+
+        /**
+        * super simple slideshow
+        * animation between slides happens with css transitions
+        */
+        function Slideshow(container, overview)
+        {
+            container = $(container);
+            overview = $(overview);
+
+            var slides = $(">li", container);
+            var width = container.parent().width();
+
+            var self = this;
+            var current = 0;
+            var total_slides = slides.length;
+
+            // overview dots
+            overview.click(function(ev) {    
+                self.slideTo( $(this).index() );
+                ev.preventDefault();
+            });
+
+            this.updateOverview = function() {
+                overview.removeClass("active");
+                $(overview.get( current )).addClass('active');
+            };
+            self.updateOverview();
+
+
+            // slide to given index
+            this.slideTo = function( index ) {
+                if(index > total_slides-1) {
+                    index = total_slides-1;
+                } 
+                else if(index < 0) {
+                    index = 0;
+                } 
+
+                if(index == current) {
+                    return false;
+                }
+
+                container.css({ left: 0 - (width * index) });
+                current = index;
+
+
+                self.updateOverview();
+
+                return true;
+            };
+
+            this.next = function() {
+                return this.slideTo(current+1);	
+            };
+
+            this.prev = function() {
+                return this.slideTo(current-1);	
+            };
+
+            this.getContainer = function() {
+                return container;
+            };
+
+            this.getCurrent = function() {
+                return $(slides.get(current));
+            };
+        }
+
+
+
+        var hammer = new Hammer($(".prt-slideshow-container").get(0));
+        var slideshow = new Slideshow(".prt-slideshow-container ul", "#overview li");
+
+
+        // ondrag we preview the next/prev slide
+        hammer.ondrag = function(ev) {
+            var left = 0;
+
+            // determine which direction we need to show the preview
+            if(ev.direction == 'left') {
+                left = 0 - ev.distance;
+            } else if(ev.direction == 'right') {
+                left = ev.distance;
+            }
+
+            // just move the marginLeft
+            slideshow.getContainer().css({ marginLeft: left });
+        };
+
+
+        // ondragend we will move to the next/prev slide when we have 
+        // opened it more then 100px on the screen
+        hammer.ondragend = function(ev) {
+            // restore the margin
+            slideshow.getContainer().css({ marginLeft: 0 });
+                
+                
+            
+            // if we moved the slide 100px then navigate
+            if(Math.abs(ev.distance) > 100) {
+                if(ev.direction == 'right') {
+                    slideshow.prev();
+                } else if(ev.direction == 'left') {
+                    slideshow.next();
+                }
+            }
+        };
+
+    
         
 });
